f4c71159de261d359f9e598880c16b2482106f74
HuaiHsin <huaihsin@gmail.com>
2018-12-28 22:32:39 +0800

    refactor

--- products.py
+++ products.py
@@ -1,53 +1,51 @@
-from datetime import date
 import os # operating system
 
-today = str(date.today())
-
-print("這是個記帳軟體，請依照指示輸入，或輸入q離開。")
-products = []
-#檢查檔案並讀取檔案
-if os.path.isfile("products.csv"):
-	with open("products.csv", "r", encoding= "utf-8") as f:
-		for line in f:
-			if "日期, 商品, 價格" in line:
-				continue
-			date, name, price = line.strip().split(",")
-			products.append([date, name, price])
-	print(products)
-else:
-	print("找不到現存檔案！")
+#讀取檔案
+def read_file(filename):
+    products = []
+    with open(filename, "r", encoding= "utf-8") as f:
+        for line in f:
+            if "商品,價格" in line:
+                continue #繼續
+            name, price = line.strip().split(",")
+            products.append([name, price])
+    return products
 
 #讓使用者輸入
-while True:
-	date = input("請輸入日期(EX, 2018-01-01)，或按Enter直接取得今天日期： ")
-	if date == "q":
-		break
-	elif date == "":
-		date = today
-	print("你輸入的是 ", date)
-	name = input("接著請輸入商品名稱： ")
-	if name == "q":
-		break
-	elif name == "":
-		name = "未購買"
-	print("你輸入的是 ", date, ":", name)
-	price = input("最後請輸入商品價格： ")
-	if price == "q":
-		break
-	elif price == "":
-		price = 0
-	price = int(price)
-	products.append([date, name, price])
-	print("你輸入的是 ", date, ":", name, "的價格是", price, "元")
-	print("\n")
+def user_input(products):
+    while True:
+        name = input("接著請輸入商品名稱： ")
+        if name == "q":
+            break
+        price = input("最後請輸入商品價格： ")
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
-print(products)
+#印出所有購買紀錄
+def print_products(products):
+    for p in products:
+        print(p[0], "的價格是", p[1], "元")
 
-for p in products:
-	print(p[0], p[1], "的價格是", p[2], "元")
+#寫入檔案
+def write_file(filename, products):
+    with open(filename, "w", encoding= "utf-8") as f:
+        f.write("商品,價格\n")
+        for p in products:
+            f.write(p[0] + "," + str(p[1]) + "\n")
+            #write 通常會跟著 \n
 
-with open("products.csv", "w", encoding= "utf-8") as f:
-	f.write("日期, 商品, 價格\n")
-	for p in products:
-		f.write(p[0] + "," + p[1] + "," + str(p[2]) + "\n")
-		#write 通常會跟著 \n
+def main():
+    filename = "products.csv"
+    if os.path.isfile(filename):
+        print("找到檔案！")
+        products = read_file(filename)
+    else:
+        print("找不到現存檔案！")
+
+    products = user_input(products)
+    print_products(products)
+    write_file("products.csv", products)
+
+main()
